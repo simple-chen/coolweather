cc60520a7e22f23ed7feb06e6e9069a1a2bf540d
clp <767369640@qq.com>
2020-01-23 21:38:39 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,43 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id ;
+    private String cityName;//城市名字
+    private int cityCode;//城市代号
+    private int provinceId;//当前城市所属省的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
